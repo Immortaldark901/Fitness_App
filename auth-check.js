089ae5b523266a02094385d1f334113243f534f4
Immortaldark901 <urvakshbhagwager@gmail.com>
2025-11-09 00:14:15 +0530

    Add authentication check script

---
+++ auth-check.js
@@ -0,0 +1,30 @@
+// Auth Check - Redirect to signup if user is not authenticated
+(function() {
+  // Function to check if user is authenticated
+  function checkAuth() {
+    const user = localStorage.getItem('user');
+    
+    // Get current page name
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    
+    // Pages that don't require authentication
+    const publicPages = ['signup.html', 'signin.html', 'home.html', 'index.html', ''];
+    
+    // Check if current page is public
+    const isPublicPage = publicPages.some(page => 
+      currentPage === page || currentPage === ''
+    );
+    
+    // If not on a public page and user is not authenticated, redirect to signup
+    if (!isPublicPage && !user) {
+      console.log('User not authenticated, redirecting to signup page');
+      window.location.href = 'signup.html';
+      return false;
+    }
+    
+    return true;
+  }
+  
+  // Run auth check when page loads
+  checkAuth();
+})();
